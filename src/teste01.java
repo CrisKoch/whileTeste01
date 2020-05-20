1eab4c9d50dcef44e4c535af0135862e85544075
CrisKoch <cristinak.iftm@gmail.com>
2020-05-20 19:45:48 -0300

    Leitura da largura com validação

--- src/teste01.java
+++ src/teste01.java
@@ -6,15 +6,26 @@ public class teste01 {
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		
-		
-		
-		
-		
-		
-		
-		
-		
+
+		// Leitura da largura com validação
+
+		double largura, altura;
+		largura = 0;
+		altura = 0;
+
+		System.out.printf("Digite a largura do retângulo: ");
+		largura = sc.nextDouble();
+		while (largura <= 0) {
+			System.out.printf("O valor deve ser positivo. Tente novamente: ");
+			largura = sc.nextDouble();
+		}
+		System.out.printf("Digite a altura do retângulo: ");
+		altura = sc.nextDouble();
+		while (altura <= 0) {
+			System.out.printf("O valor deve ser positivo. Tente novamente: ");
+			altura = sc.nextDouble();
+		}
+
 		sc.close();
 	}
 
